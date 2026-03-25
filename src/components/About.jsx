9ebd29ed2de91e7af199065889ee7ef5a0ec688e
Clark <clarkjacob627@gmail.com>
2026-03-25 19:35:09 +0800

    improve hero section and drawer component

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import styles from '../css/About.module.css';
+
+export default function About() {
+    return (
+        <>
+            <section className={styles.aboutSection} id='about'>
+                <h1>About me</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quas maxime vitae illum impedit blanditiis dolore quo reprehenderit vero. Nihil voluptas atque fugit animi fugiat sequi mollitia excepturi, voluptatem quos.</p>
+            </section>
+        </>
+    )
+}
