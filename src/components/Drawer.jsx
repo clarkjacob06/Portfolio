2de7d21819ce749c32e1b122f0e3775aa44579fa
Clark <clarkjacob627@gmail.com>
2026-04-02 19:38:35 +0800

    modify drawer UI

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -2,8 +2,43 @@ import styles from '../css/Drawer.module.css';
 
 import { X } from 'lucide-react'
 import { FileDown } from 'lucide-react';
+import gsap from 'gsap';
+import {useGSAP} from '@gsap/react';
 
 export default function Drawer({ setIsOpen }) {
+
+    useGSAP(() => {
+        const tl = gsap.timeline();
+
+        tl.fromTo(`.${styles.profileContainer}`,
+            {
+                y: -100,
+                x: 20
+            }, {
+                y: 0,
+                ease: 'bounce.out',
+                duration: 1
+            })
+            .to(`.${styles.profileContainer}`, 
+                {
+                    x: -20,
+                    ease: 'power1.out',
+                    duration: .3
+                }
+            )
+            .fromTo(`.${styles.statusText}`,
+                {
+                    scaleX: 0,
+                },
+                {
+                    scaleX: 1,
+                    transformOrigin: 'left',
+                    duration: .1,
+                    ease: 'power1.out'
+                },
+                '-=.2'
+            )
+    });
 
     return (
         <div className={styles.drawer}>
@@ -12,11 +47,14 @@ export default function Drawer({ setIsOpen }) {
             <section className={styles.messageSection}>
                 <div className={styles.profileContainer}>
                     <img src="/pixel_transparent.avif" alt="profile photo"/>
+                    <div className={styles.status}>
+                        <div className={styles.greenIcon}></div>
+                        <div className={styles.statusText}>Available to work</div>
+                    </div>
                 </div>
 
                 <a href="/resume.pdf" download={'Clark_Jacob_CV'}> <FileDown className={styles.fileIcon} />Download CV</a>
             </section>
-
             <section className={styles.navSection} >
                 <a href="#" onClick={() => setIsOpen(false)}>Home</a>
                 <a href="#project" onClick={() => setIsOpen(false)}>Projects</a>
@@ -25,4 +63,4 @@ export default function Drawer({ setIsOpen }) {
             </section>
         </div>
     )
-}
+}9
