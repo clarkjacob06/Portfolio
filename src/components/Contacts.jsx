955f197f38e8725189c81e6f175924c678f50326
Clark <clarkjacob627@gmail.com>
2026-04-23 19:39:18 +0800

    modify contacts component

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,4 +1,5 @@
 import styles from '../css/Contacts.module.css';
+import { Phone, Mail, Github, Linkedin } from 'lucide-react';
 
 export default function Contacts() {
     return(
@@ -6,9 +7,23 @@ export default function Contacts() {
         <div className={styles.contactsContainer}>
             <div className={styles.socialsContainer}>
 
+                <div className={styles.container}>
+                    <Phone className={styles.icon}/>
+                     <p>(+63) 9354116879</p>
+                </div>
+
+                <div className={styles.container}>
+                    <Mail className={styles.icon}/>
+                    <p>clarkjacob627@gmail.com</p>
+                </div>
+
+                <div className={styles.container}>
+                    <Linkedin className={styles.icon}/>
+                    <p>Clark Jacob</p>
+                </div>
             </div>
             
-            <iframe className={styles.map} allow="geolocation" src="https://api.maptiler.com/maps/base-v4/?key=83xgVCacaBjMMFKdUZvv&marker=121.0959822102268,14.596898420774755#15/14.596898420774755/121.0959822102268"></iframe>
+            <iframe className={styles.map} allow="geolocation" src="https://api.maptiler.com/maps/base-v4/?key=83xgVCacaBjMMFKdUZvv&marker=121.0959822102268,14.596898420774755#15/14.596898420774755/121.0959822102268" ></iframe>
         </div>
     )
 }   
